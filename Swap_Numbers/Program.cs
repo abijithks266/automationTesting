4cbe3c4c3042bc215c249930ae6a134481ddd0ee
abhijith.ks <abijithks266@gmail.com>
2023-02-13 15:32:04 +0530

    First

---
+++ Swap_Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter the first no");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second no");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int c = a;
+a = b;
+b = c;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
